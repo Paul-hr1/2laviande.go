f617948c01d89519f3c80eabba5bcef58683c4b8
Thoomas-nrv <thomasmorvan082@gmail.com>
2025-04-28 11:10:23 +0200

    Co-authored-by: Alexandreblot jj<Alexandreblot@users.noreply.github.com>

--- menu.js
+++ menu.js
@@ -3,14 +3,14 @@ function displayDateTime(){ // fonction pour affiche la date et l'heure
     const options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };//parametres date
     let date = event.toLocaleDateString('fr-FR',options);
     let heure = event.toLocaleTimeString('fr-FR');
-    document.getElementById("date").innerText= date; // affiche du texte dans le html à id=date
+   // document.getElementById("date").innerText= date; // affiche du texte dans le html à id=date
     document.getElementById("heure").innerText = heure;
 
     console.log(heure);
 } 
 
 function delay(){
-    let produit = documen.getElementById("produits");
+    let produit = document.getElementById("produits");
     
 }
 function loader(){
