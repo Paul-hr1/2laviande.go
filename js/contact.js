af3130cbfeffed0f182e422ff035cb2985797c9d
Paul-hr1 <paulhuray76@gmail.com>
2025-05-24 12:08:24 +0200

    contact

--- js/contact.js
+++ js/contact.js
@@ -1,2 +1,35 @@
-//fonction confirmer est dans global.js
+// A FINIR
 
+function validationChoix() {
+    const form = document.getElementById('Form');
+    const erreur = document.getElementById('erreurChoix');
+    const erreurPrenom = document.getElementById('erreurPrenom');
+    const choix = form.elements['choix'];
+    const prenom = form.elements['prenom'];
+
+
+    form.addEventListener('submit', function(e) {
+        if(prenom.value == "")
+        
+        
+        let checked = false;
+        for(let i = 0; i < choix.length; i++) {
+            if (choix[i].checked) checked = true;
+        }
+        if (!checked) {
+            e.preventDefault();
+            erreur.style.visibility = 'visible';
+        } else {
+            erreur.style.visibility = 'hidden';
+        }
+    });
+}
+
+function validationPrenom(){
+
+}
+
+function main(){
+    document.addEventListener('DOMContentLoaded', validationChoix);
+}
+main();    
