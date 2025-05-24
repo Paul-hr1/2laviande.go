5eba521641c341ae4300d80fad996dd2e62e6196
asma-abc <148458412+asma-abc@users.noreply.github.com>
2025-05-24 03:05:57 +0200

    confirmer

--- js/contact.js
+++ js/contact.js
@@ -1,6 +1,2 @@
-function confirmer(page){
-    if (confirm("Voulez-vous vraiment naviguer vers la présentation de l'équipe ?")) {
-        window.location.href = page;
-      }
-}
+//fonction confirmer est dans global.js
 
