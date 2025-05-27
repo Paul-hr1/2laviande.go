c930219d9c3865414dda611abac687a347b9ea62
Paul-hr1 <paulhuray76@gmail.com>
2025-05-27 10:02:34 +0200

    gdgd

--- js/contact.js
+++ js/contact.js
@@ -34,42 +34,42 @@ function configurerValidationFormulaire() { // fonction qui vérifie si le formu
             erreurMail.style.visibility = 'visible'; //affiche le message d'erreur
             formulaireValide = false; // met le formulaire invalide
         }
-    else{
-        erreurMail.style.visibility = 'hidden'; // cache le message d'erreur
-    }
+        else{
+            erreurMail.style.visibility = 'hidden'; // cache le message d'erreur
+        }
 
-    if(commentaire.value.length < 20 || commentaire.value.length > 1000){ // vérifie que le commentaire est compris entre 20 et 1000 caractères
-        erreurComment.style.visibility = 'visible'; // affiche le message d'erreur
-        formulaireValide = false; // met le formulaire invalide
-    }
-    else{
-        erreurComment.style.visibility = 'hidden'; // cache le message d'erreur
-    }
+        if(commentaire.value.length < 20 || commentaire.value.length > 1000){ // vérifie que le commentaire est compris entre 20 et 1000 caractères
+            erreurComment.style.visibility = 'visible'; // affiche le message d'erreur
+            formulaireValide = false; // met le formulaire invalide
+        }
+        else{
+            erreurComment.style.visibility = 'hidden'; // cache le message d'erreur
+        }
 
-    const choix = document.querySelectorAll('input[name="choix"]'); // vérifie qu'un bouton radio est coché
-    let choixValide = false; // aucun bouton n'est coché
-    choix.forEach(function (radio) { // parcours les boutons
+        const choix = document.querySelectorAll('input[name="choix"]'); // vérifie qu'un bouton radio est coché
+        let choixValide = false; // aucun bouton n'est coché
+        choix.forEach(function (radio) { // parcours les boutons
         if (radio.checked) { // si bouton coché
             choixValide = true; // validé
         }
         });
 
-    if (choixValide == false) { // si aucun bouton cohché
-        erreurChoix.style.visibility = 'visible'; // affiche le message d'erreur
-        formulaireValide = false; // met le formulaire invalide
-    } else {
-        erreurChoix.style.visibility = 'hidden'; // cache le message d'erreur
-    }
+        if (choixValide == false) { // si aucun bouton cohché
+            erreurChoix.style.visibility = 'visible'; // affiche le message d'erreur
+            formulaireValide = false; // met le formulaire invalide
+        } else {
+            erreurChoix.style.visibility = 'hidden'; // cache le message d'erreur
+        }
 
-    if (formulaireValide == false) { // si formulaire invalide
-        event.preventDefault(); // bloque l'action par défaut
-        document.getElementById('submit').disabled = true; // désactive le bouton envoyer
-    }
-    else{
-        document.getElementById('submit').disabled = false; // active le bouton envoyer
-    }
+        if (formulaireValide == false) { // si formulaire invalide
+            event.preventDefault(); // bloque l'action par défaut
+            document.getElementById('submit').disabled = true; // désactive le bouton envoyer
+        }
+        else{
+            document.getElementById('submit').disabled = false; // active le bouton envoyer
+        }
     
-  });
+    });
 }
 
 document.addEventListener('DOMContentLoaded', configurerValidationFormulaire); // quand DOM chargé lancer la fonction
